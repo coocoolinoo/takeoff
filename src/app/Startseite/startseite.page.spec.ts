0d8645b5c8e80f38c71eb5291c67ce08c199d32a
200006 <200006@studierende.htl-donaustadt.at>
2025-04-08 13:29:53 +0200

    TAK-32 Startseite erstellen

--- src/app/Startseite/startseite.page.spec.ts
+++ src/app/Startseite/startseite.page.spec.ts
@@ -1,13 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Tab1Page } from './tab1.page';
+import { StartseitePage } from './startseite.page';
 
 describe('Tab1Page', () => {
-  let component: Tab1Page;
-  let fixture: ComponentFixture<Tab1Page>;
+  let component: StartseitePage;
+  let fixture: ComponentFixture<StartseitePage>;
 
   beforeEach(async () => {
-    fixture = TestBed.createComponent(Tab1Page);
+    fixture = TestBed.createComponent(StartseitePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
