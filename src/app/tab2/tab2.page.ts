41fecc98b0c5b2a40da873b8e21ba11c4e9f0a66
Corneliu <secrieri.corneliu@gmail.com>
2025-05-12 09:03:01 +0200

    TAK 55 - Profil-Seite Login Menüs erstellen TAK 56 - Profil-Seite Login möglich machen

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,89 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import {FooterPage} from "../footer/footer.page";
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonToast
+} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, FooterPage]
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonToast,
+    FormsModule,
+    CommonModule
+  ]
 })
 export class Tab2Page {
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  };
 
-  constructor() {}
+  isRegistered = false;
+  showError = false;
 
+  constructor() {
+    this.checkRegistration();
+  }
+
+  checkRegistration() {
+    const userData = localStorage.getItem('currentUser');
+    if (userData) {
+      this.user = JSON.parse(userData);
+      this.isRegistered = true;
+    }
+  }
+
+  registerUser() {
+    if (!this.user.firstName || !this.user.lastName || !this.user.email || !this.user.password) {
+      this.showError = true;
+      return;
+    }
+
+    try {
+      localStorage.setItem('currentUser', JSON.stringify(this.user));
+      this.isRegistered = true;
+      this.showError = false;
+    } catch (e) {
+      this.showError = true;
+      console.error('Registration error:', e);
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.isRegistered = false;
+    this.user = { firstName: '', lastName: '', email: '', password: '' };
+  }
 }
